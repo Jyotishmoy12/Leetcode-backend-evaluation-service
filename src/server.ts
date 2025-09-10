ac8e0f340226d7a665e91de05ed8b3d49416a568
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-09-10 19:00:51 +0530

    added basic utility to run python code inside the container

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
-import express from 'express';
-import { serverConfig } from './config';
-import v1Router from './routers/v1/index.router';
-import v2Router from './routers/v2/index.router';
-import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
-import logger from './config/logger.config';
-import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
-import { startWorkers } from './workers/evaluation.worker';
+import express from "express";
+import { serverConfig } from "./config";
+import v1Router from "./routers/v1/index.router";
+import v2Router from "./routers/v2/index.router";
+import {
+  appErrorHandler,
+  genericErrorHandler,
+} from "./middlewares/error.middleware";
+import logger from "./config/logger.config";
+import { attachCorrelationIdMiddleware } from "./middlewares/correlation.middleware";
+import { startWorkers } from "./workers/evaluation.worker";
+//import { runPythonCode } from "./utils/containers/pythonRunner.utils";
 //import { pullAllImages } from './utils/containers/pullImage.utils';
 const app = express();
 
@@ -16,9 +20,8 @@ app.use(express.json());
  */
 
 app.use(attachCorrelationIdMiddleware);
-app.use('/api/v1', v1Router);
-app.use('/api/v2', v2Router); 
-
+app.use("/api/v1", v1Router);
+app.use("/api/v2", v2Router);
 
 /**
  * Add the error handler middleware
@@ -27,11 +30,16 @@ app.use('/api/v2', v2Router);
 app.use(appErrorHandler);
 app.use(genericErrorHandler);
 
-
 app.listen(serverConfig.PORT, async () => {
-    logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
-    logger.info(`Press Ctrl+C to stop the server.`);
-    await startWorkers();
-    logger.info("Workers started successfully");
-    //await pullAllImages();
+  logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
+  logger.info(`Press Ctrl+C to stop the server.`);
+  await startWorkers();
+  logger.info("Workers started successfully");
+  //await pullAllImages();
+  //await testPythonCode()
 });
+
+// async function testPythonCode() {
+//   const pythonCode = `print("bye")`;
+//   await runPythonCode(pythonCode);
+// }
